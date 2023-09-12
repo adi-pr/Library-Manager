9c074006e0ebd6689f3c4cfe2ee29a0d517d7cc7
Aditya Prasad <adityaprasad883@gmail.com>
2023-09-12 16:17:01 -0400

    First Commit

---
+++ Libaray.py
@@ -0,0 +1,73 @@
+from abc import ABCMeta, abstractclassmethod
+from database import Database
+from validation import DataValidator
+
+class InputSource(metaclass=ABCMeta):
+    @abstractclassmethod
+    def form_input(cls):
+        pass
+
+
+class Library:
+    def __init__(self, database):
+        self.database = database
+        self.validator = DataValidator()
+
+    def addBook(self):
+        book = Book.form_input()
+        if self.validator.validate_book(book):
+            self.database.writeBook(book)
+            self.database.getBooks()
+        else:
+            return
+
+    def getAllBooks(self):
+        return self.database.getBooks()
+
+    def removeBook(self, id):
+        self.database.removeBook(id)
+
+    def addMember(self):
+        member = Member.form_input()
+
+        if self.validator.validate_member(member):
+            self.database.writeMember(member)
+            self.database.getMembers()
+        else:
+            return
+
+class Book(InputSource):
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @classmethod
+    def form_input(cls):
+        title = input("Title of Book: ")
+        author = input("Author of Book: ")
+        return cls(title, author)
+
+
+class Member(InputSource):
+    def __init__(self, firstName, lastName, gender, emailAddress, phoneNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.gender = gender
+        self.emailAddress = emailAddress
+        self.phoneNumber = phoneNumber
+
+    @classmethod
+    def form_input(cls):
+        firstName = input("First Name: ")
+        lastName = input("Last Name: ")
+        gender = input("Gender (M, F, O): ")
+        emailAddress = input("Email: ")
+        phoneNumber = input("Phone Number: ")
+        return cls(firstName, lastName, gender, emailAddress, phoneNumber)
+
+
+if __name__ == "__main__":
+    database = Database()
+    library = Library(database)
+
+    library.addMember()
